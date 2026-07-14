23a5ab00640e1ea37d80e764c46f42f77dbb8902
Vidit Bhardwaj <viditbhardwaj123@gmail.com>
2026-07-14 09:55:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// Simple animation on scroll
+
+window.addEventListener("scroll",()=>{
+
+let cards=document.querySelectorAll(".card");
+
+
+cards.forEach(card=>{
+
+let position=card.getBoundingClientRect().top;
+
+
+if(position < window.innerHeight-100){
+
+card.style.opacity="1";
+card.style.transform="translateY(0)";
+
+}
+
+});
+
+
+});
